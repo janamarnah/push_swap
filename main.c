fae23de877d352bc6b793a414a9665161132ac21
janamarnah <amarnahjana@gmail.com>
2025-12-23 15:36:10 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,15 +1,43 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: jalamarn <jalamarn@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/12/20 18:03:46 by jalamarn          #+#    #+#             */
-/*   Updated: 2025/12/20 20:15:04 by jalamarn         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+int main(int argc, char **argv)
+{
+	t_list  **t;
+	t_list  **b;
+	t_list  *node;
+	t_list  *tmp;
+	int     args;
 
-#include "push_swap.h"
-
-
+	tmp = (*t);
+	args = 0;
+	if (argc <= 1)
+		return (0);
+	while (argv)
+	{
+		node = (add_node(atolong(argv[args])));
+		node_tolist(node, t);
+		args++;
+	}
+	args = 0;
+	while (argv)
+	{
+		if (!isdigit(argv[args]))
+			return (write(2, "Error\n", 6));
+		if (!ismaxormin(argv[args]))
+			return (write(2, "Error\n", 6));
+		if (!counter(argv[args]))
+			return (write(2, "Error\n", 6));
+		if (!dupnum(argv[args]))
+			return (write(2, "Error\n", 6));
+		args++;
+	}
+	if (listsize(t) <= 5 && listsize(t) > 1)
+	{
+		if (listsize(t) = 2)
+			twonum(t);
+		if (listsize(t) = 3)
+			threenum(t);
+		if (listsize(t) = 4)
+			fournum(t, b);
+		 if (listsize(t) = 5)
+            fivenum(t, b);
+	}
+}
